a2c5338a0f1df1261313994b448f730ac959f7db
Michael <mwhitman95@gmail.com>
2016-03-07 07:29:36 +0000

    Mostly/all the way done with mp3

--- source_js/app.js
+++ source_js/app.js
@@ -1,5 +1,20 @@
 var app = angular.module('mp3',['ngRoute']);
 
 app.config(function ($routeProvider) {
-
+	$routeProvider
+	 	.when('/', {
+	 		templateUrl: 'partials/list.html',
+	 		controller: 'ListCtrl'
+	 	})
+	 	.when('/gallery', {
+	 		templateUrl: 'partials/gallery.html',
+	 		controller: 'GalleryCtrl'
+	 	})
+	 	.when('/details/:rank', {
+	 		templateUrl: 'partials/details.html',
+	 		controller: 'DetailsCtrl'
+	 	})
+	 	.otherwise({
+	 		redirectTo: '/'
+	 	});
 })
--- source_js/controllers.js
+++ source_js/controllers.js
@@ -1,1 +1,43 @@
 /* Sample Controller */
+app.factory('helper', function($http) {
+  return {
+  	getMovies: function() {
+  		return $http.get('../data/imdb250.json')
+		.then(function(data) {
+			return data.data; //data is just has request. data.data is the list of movies
+		})
+		.catch(function(err) {
+			console.log(err);
+		});
+  	}
+  };
+});
+
+
+app.controller('ListCtrl', ['$scope', '$http', 'helper', function($scope, $http, helper) {
+   	helper.getMovies().then(function(data) {
+   		$scope.movies = data;
+	    $scope.order = false;
+	    $scope.sortTypes=[
+	    	{val: 'title', text: 'Movie Title'},
+	    	{val: 'rank', text: 'IMDB Rank'}
+	    ];
+   	});
+}]);
+
+app.controller('GalleryCtrl', ['$scope', '$http', 'helper', function($scope, $http, helper) {
+    helper.getMovies($scope, $http).then(function(data) {
+   		$scope.movies = data;
+    })
+}]);
+
+app.controller('DetailsCtrl', ['$scope', '$http', '$routeParams', '$filter', 'helper', function($scope, $http, $routeParams, $filter, helper) {
+    num = parseInt($routeParams.rank);
+    if(num === null || num < 1 || num > 250) return; //Invalid input
+    helper.getMovies().then(function(data) {
+    	$scope.movies = data;
+	    $scope.movie = $filter('filter')(data, {rank: num})[0]; //Get the movie of given rank
+	    $scope.prev = num - 1;
+	    $scope.next = num + 1;
+    });
+}]);
